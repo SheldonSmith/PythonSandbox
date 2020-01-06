7af355d7899ed4caa57b8207af5ed4d101904eab
Sheldon Smith <sheldon.john.smith@gmail.com>
2020-01-05 21:39:07 -0500

    updates to timer thread

--- timer.py
+++ timer.py
@@ -1,20 +1,36 @@
 import time as t
 from _thread import start_new_thread
+from threading import Thread
     
 class GameClock(object):
     
     def __init__(self, name):
         self.name = name
-        print(self.name)
-        self.time = 15
-        start_new_thread(self.timer_thread(1), (1,))
+        print(t.time())
+        self.time = 100
+        Thread(target=self.timer_thread).start()
 
-    def timer_thread(self, x):
-        print("timer_thread: ", x)
+    def timer_thread(self):
+        now = t.time()
+        print(f'now: {t.time}')
         while self.time > 0:
             print(self.time)
-            t.sleep(1)
+            t.sleep(0.1)
             self.time -= 1
-        print("timer ended.")
-        
-GameClock("New Game")
+        print("timer ended.", t.time()-now)
+
+def timer_thread():
+    now = float(t.time())
+    print('now: ', float(t.time()))
+    x = 20
+    while x > 0:
+        print(x)
+        t.sleep(0.1)
+        x -= 1
+    print("timer ended.", t.time()-now)     
+
+GameClock("test")
+GameClock("test2")
+#start_new_thread(timer_thread, (20,))
+#Thread(target=timer_thread).start()
+#Thread(target=timer_thread).start()
